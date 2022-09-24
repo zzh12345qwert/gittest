cbad1331f34ea473ee07bc4a61a00533c30ae297
zzh <zzh@hp.com>
2022-09-24 15:34:16 +0800

    Idea third commit

--- src/main/java/com/zzh/git/GitTest.java
+++ src/main/java/com/zzh/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello git2222");
+        System.out.println("hello git 3333333");
     }
 }
