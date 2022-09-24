a5c735cd295a0a50a10ded9234ae91959d11f355
zzh <zzh@hp.com>
2022-09-24 15:32:43 +0800

    Idea second commit

--- src/main/java/com/zzh/git/GitTest.java
+++ src/main/java/com/zzh/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zzh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello git2222");
     }
 }
